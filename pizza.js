be2ed0aec6e9ce79469beb69e54e16f8a921ee0a
numbo1! <ellenmoltumyr@gmail.com>
2024-04-10 13:34:23 +0300

    10.04.24

--- pizza.js
+++ pizza.js
@@ -5,7 +5,7 @@ let boardHeight = window.innerHeight;
 let context;
 
 //Freddy
-let freddyWidth = 60;
+let freddyWidth = 100;
 let freddyHeight = 150;
 let freddyX = boardWidth/9;
 let freddyY = boardHeight/2
@@ -19,13 +19,43 @@ let freddy = {
 
 }
 
+//Plum Freddy
+let freddyPWidth = 100;
+let freddyPHeight = 150;
+let freddyPX = boardWidth/1.1;
+let freddyPY = boardHeight/2
+let freddyPImg;
+
+let freddyP = {
+    x : freddyPX,
+    y : freddyPY,
+    width : freddyPWidth,
+    height : freddyPHeight
+}
+
+//Pizzas
+let randomNumber = parseInt(Math.round(Math.random() * 2) + Math.random() + 1);
+let pizzaArray = ["/photos/pepPizza.png"]
+let pizzaHeight = 70;
+let pizzaWidth = 70;
+let pizzaX = boardWidth/randomNumber;
+let pizzaY = boardHeight/randomNumber;
+let pizzaImg;
+
+let pizza = {
+    x : pizzaX,
+    y : pizzaY,
+    width : pizzaWidth,
+    height : pizzaHeight
+}
+
 //physics
 let velocityX = 0; 
 let velocityY = 0;
+let velocityXP = 0;
+let velocityYP = 0;
 let gameOver = false;
 let score = 0;
-
-
 
 window.onload = function(){
     board = document.getElementById("board");
@@ -34,14 +64,41 @@ window.onload = function(){
     context = board.getContext("2d"); //Drawing on the canvas
 
     //load the images
-freddyImg = new Image();
-freddyImg.src = "/photos/temporary_freddy.png";
-freddyImg.onload = function(){
-    context.drawImage(freddyImg, freddy.x, freddy.y, freddy.width, freddy.height);
+    //Freddy
+    freddyImg = new Image();
+    freddyImg.src = "/photos/freddy.png";
+    freddyImg.onload = function(){
+        context.drawImage(freddyImg, freddy.x, freddy.y, freddy.width, freddy.height);
 
-}
+    }
 
-document.addEventListener("keydown", moveFreddy);
+    //Freddy P
+    freddyPImg = new Image();
+    freddyPImg.src = "/photos/freddyPurple.png";
+    freddyPImg.onload = function(){
+        context.drawImage(freddyPImg, freddyP.x, freddyP.y, freddyP.width, freddyP.height);
+
+    }
+
+    //Pizzas
+    pizzaImg = new Image();
+    pizzaImg.src = "/photos/pepPizza.png";
+    pizzaImg.onload = function(){
+        context.drawImage(pizzaImg, pizza.x, pizza.y, pizza.width, pizza.height);
+
+    }
+
+
+    function stopMoving() {
+            velocityY = 0;
+            velocityX = 0;
+            velocityYP = 0;
+            velocityXP = 0;
+    }
+    requestAnimationFrame(update);
+    document.addEventListener('keydown', keyPressed);
+    document.addEventListener('keyup', stopMoving);
+
 }
 
 function update() {
@@ -54,17 +111,38 @@ function update() {
     //Freddy
     freddy.y += velocityY;
     freddy.y = Math.max(freddy.y + velocityY, 0);
-    context.drawImage(austronautImg, austronaut.x, austronaut.y, austronaut.width, austronaut.height);
+    freddy.y = Math.min(freddy.y + velocityY, 890)
+    freddy.x += velocityX;
+    freddy.x = Math.max(freddy.x + velocityX, 0);
+    freddy.x = Math.min(freddy.x + velocityX, 1860);
+    context.drawImage(freddyImg, freddy.x, freddy.y, freddy.width, freddy.height);
 
-    if (freddy.y > board.height) {
-        gameOver = true;
-}
+    //Freddy P
+    freddyP.y += velocityYP;
+    freddyP.y = Math.max(freddyP.y + velocityYP, 0);
+    freddyP.y = Math.min(freddyP.y + velocityYP, 890)
+    freddyP.x += velocityXP;
+    freddyP.x = Math.max(freddyP.x + velocityXP, 0);
+    freddyP.x = Math.min(freddyP.x + velocityXP, 1860);
+    context.drawImage(freddyPImg, freddyP.x, freddyP.y, freddyP.width, freddyP.height);
+
+    // Draw pizza only if not collided
+    if (!detectCollision(freddy, pizza)) {
+        context.drawImage(pizzaImg, pizza.x, pizza.y, pizza.width, pizza.height);
+    } else {
+        // Increment score and regenerate pizza
+        score += 1;
+        pizza.x = Math.random() * (boardWidth - pizzaWidth);
+        pizza.y = Math.random() * (boardHeight - pizzaHeight);
+    }
+
+    // Draw score
+    context.font = "70px Arial";
+    context.fillStyle = "red";
+    context.fillText(score, 40, 80);
 }
 
-//score
-context.fillStyle = "white";
-context.font = "80px sans-serif"
-context.fillText(score, 20, 80);
+
 
 if (gameOver) {
     context.fillText("GAME OVER", 700, 400);
@@ -72,11 +150,11 @@ if (gameOver) {
 }
 
 //Character movement
-function moveFreddy(e) {
-    switch (e.key) {
+function keyPressed(event) {
+    switch (event.key) {
         case "w":
             //Move upwards
-            velocityY = -3;
+            velocityY = -2;
 
             //reset game
             if (gameOver) {
@@ -88,15 +166,39 @@ function moveFreddy(e) {
         
         case "s":
             //Move downwards
-            velocityY = 3;
-            break;
-
-        case "d":
-            velocityX = -3;
+            velocityY = 2;
             break;
 
         case "a":
-            velocityX = 3;
+            velocityX = -2;
             break;
+
+        case "d":
+            velocityX = 2;
+            break;
+        
+        case "ArrowUp":
+            velocityYP = -1;
+            break;
+
+        case "ArrowDown":
+            velocityYP = 1;
+            break;
+
+        case "ArrowLeft":
+            velocityXP = -1;
+            break;
+
+        case "ArrowRight":
+            velocityXP = 1;
+            break;
+        
     }
 }
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
+} 
